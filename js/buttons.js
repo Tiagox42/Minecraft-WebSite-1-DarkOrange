7de558ed19130d29b4e837180d2cca1b9bf25380
Tiago Dantas <tiagodantas42@gmail.com>
2021-10-18 22:07:22 -0300

    1

---
+++ js/buttons.js
@@ -0,0 +1,8 @@
+
+function openContent1() {
+    var show1 = document.getElementById('content1');
+    if (show1.style.display == 'none') {
+        show1.style.display = 'block' } else {
+            show1.style.display = 'none'
+        }
+}
