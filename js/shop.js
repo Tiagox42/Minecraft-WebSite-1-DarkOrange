7a2ed91ebc5d0df7f807f3ebf4d6d6495dcaea5a
Tiago Dantas <tiagodantas42@gmail.com>
2021-12-29 23:16:15 -0300

    update

---
+++ js/shop.js
@@ -0,0 +1,23 @@
+function Vip1() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip2() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip3() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip4() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip5() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip6() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
